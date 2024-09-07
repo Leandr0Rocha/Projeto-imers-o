05f0aeeaf58c82ed2cbb949e65db4215de853394
Leandro Rocha <leandror_silva@outlook.com>
2024-09-06 21:10:16 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,71 @@
+let dados = [{
+    titulo: 'O estrangeiro, Albert Camus',
+    descricao: 'Meursault, um homem aparentemente indiferente aos acontecimentos à sua volta. A narrativa acompanha sua vida cotidiana, desde o recebimento da notícia da morte de sua mãe até um assassinato e seu julgamento.Através de Meursault, Camus explora temas como o absurdo da existência, a alienação e a busca por significado em um mundo que parece não oferecer respostas.',
+    link: 'https://pt.wikipedia.org/wiki/O_Estrangeiro_(livro)',
+    imagem: 'https://i1.sndcdn.com/artworks-000244848558-cyo2to-t500x500.jpg',
+    tags: 'frânces frances frança filosofia filosófico filosofico existencialismo'
+},
+{
+    titulo: 'Jane Eyre, Charlotte Brontë',
+    descricao: 'Uma órfã inteligente e determinada, Jane Eyre, encontra um emprego como governanta em Thornfield Hall. Lá, ela se apaixona por seu enigmático patrão, Edward Rochester. No entanto, o amor deles é ameaçado por segredos obscuros do passado de Rochester, incluindo um casamento anterior e uma figura misteriosa trancada em um quarto secreto. Jane é forçada a fazer escolhas difíceis entre o amor e a independência.',
+    link: 'https://pt.wikipedia.org/wiki/Jane_Eyre',
+    imagem: 'https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781625587091/jane-eyre-9781625587091_hr.jpg',
+    tags: 'inglaterra inglês ingles romance romantismo'
+},
+{
+    titulo: 'Drácula, Bram Stoker',
+    descricao: 'O advogado Jonathan Harker viaja para a Transilvânia para concluir uma transação imobiliária com o misterioso Conde Drácula. Ao chegar ao castelo do Conde, Harker se torna prisioneiro e testemunha os horrores da imortalidade de Drácula e sua sede por sangue. Com a ajuda de amigos e especialistas, um grupo de pessoas se une para caçar Drácula e pôr fim ao seu reinado de terror.',
+    link: 'https://pt.wikipedia.org/wiki/Dr%C3%A1cula',
+    imagem: 'https://cdl-static.s3-sa-east-1.amazonaws.com/covers/gg/9788537816196/dracula-edicao-bolso-de-luxo.jpg',
+    tags: 'irlanda irlândes irlandes terror gótico gotico'
+},
+{
+    titulo: 'Quincas Borba, Machado de Assis',
+    descricao: 'Uma sátira social afiada, Quincas Borba narra a história de Rubião, um homem simples que herda uma fortuna e se muda para o Rio de Janeiro. Em meio à alta sociedade, Rubião se vê envolvido em intrigas e traições, perdendo gradualmente seus bens e sua sanidade. A obra explora temas como a corrupção, a hipocrisia e a fragilidade das relações humanas, através da filosofia irônica de Quincas Borba, o criador da teoria do Humanitismo.',
+    link: 'https://pt.wikipedia.org/wiki/Quincas_Borba',
+    imagem: 'https://m.media-amazon.com/images/I/719hCd9rSLL._AC_UF894,1000_QL80_.jpg',
+    tags: 'brasil brasileiro rio realismo sátira satira machadiano'
+},
+{
+    titulo: 'Dom Quixote, Miguel de Cervantes',
+    descricao: 'Um clássico da literatura espanhola, Dom Quixote narra as aventuras de um hidalgo que, obcecado por romances de cavalaria, decide se tornar um cavaleiro errante. Acompanhado de seu fiel escudeiro Sancho Pança, Dom Quixote embarca em jornadas hilárias e tragicômicas, combatendo moinhos de vento e confundindo realidade com ficção. A obra satiriza a sociedade da época e explora temas como a loucura, a idealização e a busca pela honra.',
+    link: 'https://pt.wikipedia.org/wiki/Dom_Quixote',
+    imagem: 'https://m.media-amazon.com/images/I/51XULadddlL._SY580_.jpg',
+    tags: 'espanha espanhol cavalaria sátira'
+},
+{
+    titulo: 'Moby Dick, Herman Melville',
+    descricao: 'Uma jornada épica de obsessão e vingança, Moby Dick narra a caçada do capitão Ahab por uma baleia branca. A obra explora temas como a natureza, a loucura e a busca por significado em um mundo hostil.',
+    link: 'https://pt.wikipedia.org/wiki/Moby_Dick',
+    imagem: 'https://m.media-amazon.com/images/I/A1xWjc50fmL._AC_UF1000,1000_QL80_.jpg',
+    tags: 'estados unidos americano aventura mar náutico nautico'
+},
+{
+    titulo: 'Orgulho e Preconceito, Jane Austen',
+    descricao: 'Uma comédia romântica que satiriza a sociedade inglesa do século XIX, Orgulho e Preconceito narra a história de amor entre Elizabeth Bennet e o orgulhoso Sr. Darcy. A obra explora temas como o casamento, a classe social e a importância do julgamento de caráter.',
+    link: 'https://pt.wikipedia.org/wiki/Orgulho_e_Preconceito',
+    imagem: 'https://m.media-amazon.com/images/I/514KTwIAT4L._SY580_.jpg',
+    tags: 'inglaterra inglês ingles romance comédia comedia'
+},
+{
+    titulo: 'Os Miseráveis, Victor Hugo',
+    descricao: 'Um épico da literatura francesa, Os Miseráveis acompanha a vida de Jean Valjean, um ex-condenado que busca redenção após roubar um pão para alimentar sua família. A obra aborda temas como a justiça social, a compaixão, a revolução e o amor, através das histórias de Valjean e de outros personagens como Cosette, Fantine e Javert. Um retrato vívido da França do século XIX.',
+    link: 'https://pt.wikipedia.org/wiki/Os_Miseráveis',
+    imagem: 'https://www.lpm.com.br/livros/imagens/miseraveis_hq_9788525426666_hd.jpg',
+    tags: 'frança francês frances romance social'
+},
+{
+    titulo: 'Crime e Castigo, Fyodor Dostoyevsky',
+    descricao: 'Um romance psicológico que explora a mente de um jovem estudante que comete um assassinato e as consequências morais de seu ato. Dostoyevsky mergulha nas profundezas da alma humana, abordando temas como a culpa, o arrependimento e a busca por Deus.',
+    link: 'https://pt.wikipedia.org/wiki/Crime_e_Castigo',
+    imagem: 'https://m.media-amazon.com/images/I/81+dpv22LGL._AC_UF894,1000_QL80_.jpg',
+    tags: 'rússia russia russo psicológico psicologico suspense filosofia'
+},
+{
+    titulo: 'Frankenstein, Mary Shelley',
+    descricao: 'Um clássico da literatura gótica, Frankenstein narra a história de um cientista que cria uma criatura artificial e as consequências de sua criação. A obra explora temas como a ciência, a ética e a natureza da vida.',
+    link: 'https://pt.wikipedia.org/wiki/Frankenstein',
+    imagem: 'https://d1b6q258gtjyuy.cloudfront.net/Custom/Content/Products/04/58/0458_https-www-escala-com-br-frankenstein-mary-shelley-p2144-_m1_637951427128518564.jpg',
+    tags: 'inglaterra inglês ingles gótico gotico terror ciência ciencia ficção'
+}
+]
